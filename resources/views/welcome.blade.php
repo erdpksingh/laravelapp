cbf8c4b40b642d0ea576013f3a3bae1e142b73c8
deepak singh <deepak.singh@covalensedigital.com>
2021-09-24 12:01:39 +0530

    testing ci cd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel App
+                    Laravel App Deepak testing
                 </div>
 
                 <div class="links">
