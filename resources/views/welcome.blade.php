17561f7d17df4e24f37e15100cfb87d1066f9574
deepak singh <deepak.singh@covalensedigital.com>
2021-09-24 12:41:56 +0530

    payload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel app :)bb
+                    Laravel app :) :)
                 </div>
 
                 <div class="links">
